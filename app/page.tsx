cf3fff791caa71409e00ad14f49a04a64bbabbf3
Stupel <marian.holly7117@gmail.com>
2024-11-06 22:27:38 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -69,8 +69,9 @@ export default function Home() {
                   nedávne vybrané projekty
                 </h2>
                 <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                  Objav moju vášeň pre tvorbu interaktívnych a užívateľsky prívetivých webových
-                  zážitkov. Tu je niekoľko mojich obľúbených.
+                  Objav moju vášeň pre tvorbu interaktívnych a užívateľsky
+                  prívetivých webových zážitkov. Tu je niekoľko mojich
+                  obľúbených.
                 </p>
               </div>
             </div>
@@ -83,33 +84,37 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="w-full mt-4 md:mt-6">
+      {/* <section className="w-full mt-4 md:mt-6">
         <div className="w-full flex flex-col items-center">
           <VideoPlayer src="videos/screen-capture-op.mp4" />
-          <div className="my-4 flex flex-col items-center justify-center gap-1 w-5/6 py-2 px-4 md:px-8
-          bg-white/80 backdrop-blur-sm rounded-lg md:backdrop-filter-none shadow-xl">
+          <div
+            className="my-4 flex flex-col items-center justify-center gap-1 w-5/6 py-2 px-4 md:px-8
+          bg-white/80 backdrop-blur-sm rounded-lg md:backdrop-filter-none shadow-xl"
+          >
             <CardTitle className="text-lg flex justify-between items-center">
               Bookish Retreat
             </CardTitle>
             <p className="text-xs text-muted-foreground">Jan 2024 - Feb 2024</p>
             <p className="text-sm mb-2 line-clamp-2 ">
-              This is a mock description for the project. It showcases the project's main features
-              and goals in a concise manner.
+              This is a mock description for the project. It showcases the
+              project's main features and goals in a concise manner.
             </p>
           </div>
         </div>
-      </section>
+      </section> */}
+
+      {/* <section className="w-full mt-4 md:mt-6">
+        <div className="flex flex-col items-center p-4 bg-white dark:bg-gray-800 rounded-lg shadow-md">
+          <h2 className="text-xl font-bold mb-4">Bookish Retreat</h2>
+          <VideoPlayer src="videos/screen-capture-op.mp4" />
+        </div>
+      </section> */}
 
       <section className="w-full mt-4 md:mt-6">
-      <div className="flex flex-col items-center p-4 bg-white dark:bg-gray-800 rounded-lg shadow-md">
-      <h2 className="text-xl font-bold mb-4">Bookish Retreat</h2>
-      <VideoPlayer src="videos/screen-capture-op.mp4" />
-    </div>
+        {DATA.projects.map((project, id) => (
+          <VideoPlayer2 key={id} {...project} />
+        ))}
       </section>
-
-      <section className="w-full mt-4 md:mt-6">
-                <VideoPlayer2 src="videos/screen-capture-op.mp4" />
-              </section>
 
       <section id="repositories">
         <GitHubRepoList />
@@ -127,16 +132,20 @@ export default function Home() {
                   zdieľam, čo sa učím
                 </h2>
                 <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                  Ponorte sa do môjho blogu, kde zdieľam svoje poznatky z webového vývoja, od
-                  praktických návodov po tipy a triky pre front-end technológie ako React, Next.js a
-                  dizajnové nástroje ako Tailwind CSS a Shadcn UI.
+                  Ponorte sa do môjho blogu, kde zdieľam svoje poznatky z
+                  webového vývoja, od praktických návodov po tipy a triky pre
+                  front-end technológie ako React, Next.js a dizajnové nástroje
+                  ako Tailwind CSS a Shadcn UI.
                 </p>
               </div>
             </div>
           </BlurFade>
           <div className="">
             {DATA.featuredArticles.map((article, id) => (
-              <BlurFade delay={BLUR_FADE_DELAY * 10 + id * 0.05} key={article.title}>
+              <BlurFade
+                delay={BLUR_FADE_DELAY * 10 + id * 0.05}
+                key={article.title}
+              >
                 <BlogCard
                   id={id}
                   slug={article.slug}
@@ -157,14 +166,22 @@ export default function Home() {
               <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
                 kontakt
               </div>
-              <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">dajte mi vedieť</h2>
+              <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                dajte mi vedieť
+              </h2>
               <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
                 ani facebook, ani instagram, ale môžete ma nájsť na{" "}
-                <Link href={DATA.contact.email} className="text-cyan-600 hover:underline">
+                <Link
+                  href={DATA.contact.email}
+                  className="text-cyan-600 hover:underline"
+                >
                   LinkedIn
                 </Link>
                 . naj možnosť je{" "}
-                <Link href={DATA.contact.email} className="text-cyan-600 hover:underline">
+                <Link
+                  href={DATA.contact.email}
+                  className="text-cyan-600 hover:underline"
+                >
                   via mail
                 </Link>
                 . Ozvem sa, keď budem môcť.
