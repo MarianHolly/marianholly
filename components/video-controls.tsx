65dddb4f27b96e7fe4a27fb9458805702b1e6052
Stupel <marian.holly7117@gmail.com>
2024-11-06 22:33:26 +0100

    video controls

--- components/video-controls.tsx
+++ components/video-controls.tsx
@@ -1,63 +1,71 @@
 import React from "react";
-import { Pause as PauseButton, Play as PlayButton } from "lucide-react";
+import { TIcons } from "./icons";
+import { cn } from "@/lib/utils";
 
 interface VideoPlayerControlsProps {
-  progress: number;
   size?: number | undefined;
+  progress: number;
   width?: number | undefined;
   isPaused: boolean;
   onPlayPause: () => void;
+  trackClassName?: string;
+  progressClassName?: string;
+  className?: string;
 }
 
-
 export default function VideoPlayerControls({
-    progress,
-    size = 48,
-    width = 3,
-    isPaused,
-    onPlayPause,
-  }: VideoPlayerControlsProps ) {
-  
-    const center = size / 2;
+  progress,
+  size = 48,
+  width = 4,
+  isPaused,
+  className,
+  onPlayPause,
+  progressClassName = "text-slate-950",
+  trackClassName = "text-black/10 dark:text-white/10",
+}: VideoPlayerControlsProps) {
+  const center = size / 2;
+
   const radius = center - width;
   const dashArray = 2 * Math.PI * radius;
   const dashOffset = dashArray * (1 - progress);
 
-  
-    return (
-    
-        <div className="relative flex justify-center items-center bg-white backdrop-blur-3xl rounded-full">
-        <svg width={size} height={size} style={{ transform: "rotate(-90deg)" }}>
-          <circle
-            cx={center}
-            cy={center}
-            r={radius}
-            fill="transparent"
-            stroke="#aaaaaa"
-            strokeWidth={width}
-          />
-          <circle
-            cx={center}
-            cy={center}
-            r={radius}
-            fill="transparent"
-            stroke="#ffffff"
-            strokeWidth={width}
-            strokeDasharray={dashArray}
-            strokeDashoffset={dashOffset}
-            strokeLinecap="round"
-          />
-        </svg>
-        <div className="absolute">
-          <button
-            className="group cursor-pointer flex justify-center items-center"
-            onClick={onPlayPause}
-          >
-            <div className=" fill-[#000000] group-hover:fill-[#000000] transition-colors duration-200 ease-in-out">
-              {isPaused ? <PlayButton /> : <PauseButton />}
-            </div>
-          </button>
-        </div>
+  return (
+    <div className={`relative flex justify-center items-center ${className}`}>
+      <svg width={size} height={size} style={{ transform: "rotate(-90deg)" }}>
+        <circle
+          r={radius}
+          cx={center}
+          cy={center}
+          fill="transparent"
+          stroke="currentColor"
+          strokeWidth={width + 1}
+          strokeDasharray={"10px 0"}
+          strokeDashoffset="0px"
+          className={cn("duration-500", trackClassName)}
+        />
+        <circle
+          r={radius}
+          cx={center}
+          cy={center}
+          stroke="currentColor"
+          strokeDasharray={dashArray}
+          strokeDashoffset={dashOffset}
+          strokeLinecap="round"
+          strokeWidth={width}
+          fill="transparent"
+          className={cn("duration-500", progressClassName)}
+        />
+      </svg>
+      <div className="absolute">
+        <button
+          className="group cursor-pointer flex justify-center items-center"
+          onClick={onPlayPause}
+        >
+          <div className=" fill-[#000000] group-hover:fill-[#000000] transition-colors duration-200 ease-in-out">
+            {isPaused ? <TIcons.play className="w-4 h-4" /> : <TIcons.pause className="w-5 h-5" />}
+          </div>
+        </button>
       </div>
-  )
+    </div>
+  );
 }
